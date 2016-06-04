6ef377a50a2a9696eb8a1834e9396d6b3b1aa831
Xu Haiming <haiming12322@qq.com>
2016-06-04 13:36:23 +0800

    Add user page

--- server.js
+++ server.js
@@ -5,23 +5,18 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+app.use(express.static('public'));
+
 app.use(session({
   secret: 'test'
 }));
 
-app.get('/',function(req,res){
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
-app.post('/register', (req, res, next) => {
-  auth.registerNewUser(req.body.displayName, req.body.username, req.body.password, (error) => {
-    if (error != null) {
-      res.send(error);
-    } else {
-      res.send('hello');
-    }
-    next();
-  });
+app.get('/user', (req, res) => {
+  res.sendFile(path.join(__dirname+'/public/userPage.html'));
 });
 
 app.listen(PORT, () => {
