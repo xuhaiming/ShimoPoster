ef919adb0bebd339d627427e9e0b6fb8ed02d659
Xu Haiming <haiming12322@qq.com>
2016-06-04 12:40:33 +0800

    Add index page

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require('express');
 const session = require('express-session');
-const bodyParser = require('body-parser');
-//
-// const { blog } = require('./service/blog');
-// const { auth } = require('./service/authentication');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }));  // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                           // parse application/json
 app.use(session({
-  secret: 'test',
+  secret: 'test'
 }));
 
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+});
 
 app.post('/register', (req, res, next) => {
   auth.registerNewUser(req.body.displayName, req.body.username, req.body.password, (error) => {
@@ -25,37 +23,6 @@ app.post('/register', (req, res, next) => {
     next();
   });
 });
-
-app.get('/test', (req, res, next) => {
-  const ss = req.session;
-  const sid = req.sessionID;
-  const hour = 3600000;
-
-  ss.cookie.expires = new Date(Date.now() + hour);
-  ss.cookie.maxAge = hour;
-  if (ss.views) {
-    ss.views++;
-  } else {
-    ss.views = 1;
-  }
-  console.log(`sid ${sid}`);
-  console.log(ss);
-  res.send('hello');
-  next();
-});
-
-app.post('/createblog', (req, res, next) => {
-  blog.createNewBlog(req.body.title, req.body.date, req.body.content, (error) => {
-    console.log(req.body);
-    if (error != null) {
-      res.send(error);
-    } else {
-      res.send('ERROR');
-    }
-    next();
-  });
-});
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
